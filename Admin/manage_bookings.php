f08e866315e3d679aa0eb04f9274d565f7089029
Dimasrizky <dimasrizky822@gmail.com>
2025-01-03 09:31:13 +0700

    Update manage_bookings.php

--- Admin/manage_bookings.php
+++ Admin/manage_bookings.php
@@ -25,6 +25,7 @@ $bookings = $pdo->query($query)->fetchAll();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kelola Pemesanan - Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <link href="../Style/manage_booking.css" rel="stylesheet">
 </head>
 <body class="bg-light">
