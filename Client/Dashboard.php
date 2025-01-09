a7944288e90ffbda89e921d90646990d7ac8b391
Dimasrizky <dimasrizky822@gmail.com>
2025-01-09 09:53:37 +0700

    Update Dashboard.php

--- Client/Dashboard.php
+++ Client/Dashboard.php
@@ -242,7 +242,7 @@ if (!isLoggedIn()) {
                                     <div class="rating">
                                         <?php for($i = 5; $i >= 1; $i--): ?>
                                         <input type="radio" id="star<?php echo $i; ?>" name="rating" value="<?php echo $i; ?>" required>
-                                        <label for="star<?php echo $i; ?>">☆</label>
+                                        <label for="star<?php echo $i; ?>"></label>
                                         <?php endfor; ?>
                                     </div>
                                 </div>
